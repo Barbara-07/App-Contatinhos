6f4b176c031a880f5c6c51733c40be7d6aff577f
barbaracarvsouza07@gmail.com <barbaracarv.souza07@gmail.com>
2024-09-06 08:52:23 -0300

    Criação de Barra trasparente

--- contatinhos/src/app/home/index.tsx
+++ contatinhos/src/app/home/index.tsx
@@ -5,7 +5,9 @@ import { styles } from "./styles";
 export function Home() {
     return (
         <View style={styles.container}>
-            
+            <View style={styles.header}>
+                
+            </View>
         </View>
     )
 }
