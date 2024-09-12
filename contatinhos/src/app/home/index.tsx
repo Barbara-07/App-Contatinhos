8ab80fbabe0f15302aba39f9378d356e9db7ec49
barbaracarvsouza07@gmail.com <barbaracarv.souza07@gmail.com>
2024-09-12 13:47:43 -0300

    Imagens para contato adicionadas

--- contatinhos/src/app/home/index.tsx
+++ contatinhos/src/app/home/index.tsx
@@ -1,14 +1,33 @@
-import { View } from "react-native";
+import { Alert, View, SectionList } from "react-native";
 import { Feather } from '@expo/vector-icons'
+import { useState, useEffect } from "react";
 import { styles } from "./styles"
+import * as Contacts from 'expo-contacts'
 import { Input } from '@/app/components/input'
 import { theme } from "@/theme";
-
-import { useState } from 'react'
 import { Contact } from '@/app/components/contact'
 
 export function Home() {
-    const [name, setName] = useState("")
+
+    async function  fetchContacts() {
+        try{
+            const { status } = await Contacts.requestPermissionsAsync()
+            if (status === Contacts.PermissionStatus.GRANTED){
+                const {data} = await Contacts.getContactsAsync()
+                console.log(data)}
+            } catch(error){
+                console.log(error)
+                Alert.alert("Contatos", "Não foi possível carregar os contatos")
+            }
+        }
+    
+        const [name, setName] = useState("")
+        const [contacts, setContacts] = useState([])
+    
+        useEffect(() => {
+            fetchContacts()
+        },[])
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>
@@ -21,11 +40,16 @@ export function Home() {
                     color={theme.colors.gray_300}onPress={() => setName("")}></Feather>
                 </Input>
                 </View>
+                <SectionList
+                sections={contacts}
+                keyExtractor={( item )=> item.id} 
+                renderItem={({ item }) => (
             <Contact contact={{
                 name: "Pedroso",
                 image: require("@/assets/avatar.jpeg")
             }} />
-
+        )}
+        />
         </View>
     )
 }
